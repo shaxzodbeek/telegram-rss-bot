bb3c0d3841237e7ce04afa293e34dcb658a02d95
shaxzodbeek <91079193+shaxzodbeek@users.noreply.github.com>
2024-12-28 12:08:29 +0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import { startBot } from './bot.js';
-import { startHealthServer } from './health.js';
-
-// Start health check server for Render
-startHealthServer();
-
-// Start the bot
-startBot().catch(console.error);
