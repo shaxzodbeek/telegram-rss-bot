0b3340f15e8df62647a672cdc7187c0bf9f87506
shaxzodbeek <91079193+shaxzodbeek@users.noreply.github.com>
2025-01-06 15:03:42 +0500

    Delete src directory

--- src/constants.js
+++
@@ -1,6 +0,0 @@
-// RSS feed configuration
-export const RSS_CONFIG = {
-    FETCH_INTERVAL: 300000, // 5 minutes
-    MESSAGE_DELAY: 3000,    // 3 seconds between messages
-    SUMMARY_LENGTH: 300     // Maximum length of message summary
-};
